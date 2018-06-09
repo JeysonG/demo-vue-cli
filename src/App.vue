c00c733b4eebcfc83757c00ca2a4e022fd3394b9
Json <jsgon8@gmail.com>
2018-06-09 09:01:49 -0500

    Fixes URL gitPages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://jeysong.github.io/demo-vue-cli/dist/logo.png')
     h1 {{ msg }}
 
     select(v-model="selectedCountry")
